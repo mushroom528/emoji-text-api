8deca6df21535c56ad0a8d097eba6897f8d29ff1
mushroom528 <rlagyrnjs12@naver.com>
2024-10-17 13:16:49 +0900

    Initial commit

---
+++ src/main/java/com/emojitext/EmojiTextApiApplication.java
@@ -0,0 +1,13 @@
+package com.emojitext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiTextApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmojiTextApiApplication.class, args);
+    }
+
+}
