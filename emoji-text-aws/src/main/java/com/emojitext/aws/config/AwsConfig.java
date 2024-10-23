5965a5d65a54d9fea35723e11585196819130b5f
mushroom528 <rlagyrnjs12@naver.com>
2024-10-23 12:38:10 +0900

    chore: Rename emoji-text-application to emoji-text-core

---
+++ emoji-text-aws/src/main/java/com/emojitext/aws/config/AwsConfig.java
@@ -0,0 +1,9 @@
+package com.emojitext.aws.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.emojitext.aws")
+public class AwsConfig {
+}
